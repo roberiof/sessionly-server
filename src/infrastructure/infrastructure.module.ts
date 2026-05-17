b0623f652cbf58af51d070a483fc9e18bc994bc7
roberiof <roberiofilhodev@gmail.com>
2026-05-17 18:45:36 -0300

    feat(email): implement EmailService for sending password reset emails

--- src/infrastructure/infrastructure.module.ts
+++ src/infrastructure/infrastructure.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import {
+  PASSWORD_RESET_TOKENS_REPOSITORY,
   REFRESH_TOKENS_REPOSITORY,
   USERS_REPOSITORY,
 } from 'src/domain/repositories/tokens';
+import { EmailService } from './email/email.service';
 import { ClientProfileMapper } from './database/mappers/client-profile.mapper';
 import { MentorProfileMapper } from './database/mappers/mentor-profile.mapper';
 import { UserMapper } from './database/mappers/user.mapper';
 import { PrismaModule } from './database/prisma/prisma.module';
+import { PrismaPasswordResetTokenRepository } from './database/repositories/prisma-password-reset-token.repository';
 import { PrismaRefreshTokenRepository } from './database/repositories/prisma-refresh-token.repository';
 import { PrismaUserRepository } from './database/repositories/prisma-user.repository';
 
@@ -19,6 +22,10 @@ const REPOSITORIES = [
     provide: REFRESH_TOKENS_REPOSITORY,
     useClass: PrismaRefreshTokenRepository,
   },
+  {
+    provide: PASSWORD_RESET_TOKENS_REPOSITORY,
+    useClass: PrismaPasswordResetTokenRepository,
+  },
 ];
 
 @Module({
@@ -28,7 +35,8 @@ const REPOSITORIES = [
     UserMapper,
     MentorProfileMapper,
     ClientProfileMapper,
+    EmailService,
   ],
-  exports: [...REPOSITORIES],
+  exports: [...REPOSITORIES, EmailService],
 })
 export class InfrastructureModule {}
